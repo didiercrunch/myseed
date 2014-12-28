f6ec518814b21024b6de1256737de67529fc60d2
didier amyot <didier.amyot@gmail.com>
2014-12-27 18:29:18 -0800

    removed mongodb

--- params.go
+++ params.go
@@ -3,7 +3,6 @@ package main
 import (
 	"io/ioutil"
 
-	mgo "gopkg.in/mgo.v2"
 	goyaml "gopkg.in/yaml.v2"
 )
 
@@ -12,15 +11,9 @@ const PARAM_FILE string = "params.yml"
 type Params struct {
 	Port        int           `yaml:"port,omitempty"`
 	Host        string        `yaml:"host,omitempty"`
-	Mongo       *MongoParams  `yaml:"mongo,omitempty"`
-	MongoClient *mgo.Session  `yaml:"-"`
-	DB          *mgo.Database `yaml:"-"`
 }
 
-type MongoParams struct {
-	Url          string `yaml:"url,omitempty"`
-	DatabaseName string `yaml:"database_name,omitempty"`
-}
+
 
 func (this *Params) LoadFromYamlFile(fileName string) error {
 	data, err := ioutil.ReadFile(fileName)
@@ -43,45 +36,20 @@ func (this *Params) LoadFromYamlData(data []byte) error {
 	if p.Port > 0 {
 		this.Port = p.Port
 	}
-	if p.Mongo == nil {
-		return nil
-	}
-	if p.Mongo.Url != "" {
-		this.Mongo.Url = p.Mongo.Url
-	}
-	if p.Mongo.DatabaseName != "" {
-		this.Mongo.DatabaseName = p.Mongo.DatabaseName
-	}
-	if p.Mongo.DatabaseName != "" {
-		this.Mongo.DatabaseName = p.Mongo.DatabaseName
-	}
 
 	return nil
 }
 
-func (this *Params) initMongo() error {
-	var err error
-	this.MongoClient, err = mgo.Dial(this.Mongo.Url)
-	this.DB = this.MongoClient.DB(this.Mongo.DatabaseName)
 
-	return err
-}
 
 func GetDefaultParams() *Params {
-	mongoParams := &MongoParams{
-		Url:          "localhost:27017",
-		DatabaseName: "union",
-	}
-	return &Params{7777, "localhost", mongoParams, nil, nil}
+	return &Params{7777, "localhost"}
 }
 
 var params = GetDefaultParams()
 
 func init() {
 	if err := params.LoadFromYamlFile(PARAM_FILE); err != nil {
-		panic(err)
-	}
-	if err := params.initMongo(); err != nil {
 		panic(err)
 	}
 }
--- params_test.go
+++ params_test.go
@@ -11,20 +11,7 @@ port: 9999
 
 host: 0.0.0.0  #localhost
 
-# all the params relative to mongodb
-mongo:
-    # the url where to get mongodb
-    url: localhost:1234
-    
-    # the name of the database to use
-    database_name: geography
-    
-    # the name of the collection where all the street can be found
-    street_collection: quebec_street_map
-      
-    
-    # the collection where all the heat map point will be stored
-    heat_map_collection: quebec_heat_map  # quebec_heat_map
+
 `
 
 func TestLoadFromYamlData(t *testing.T) {
@@ -38,15 +25,6 @@ func TestLoadFromYamlData(t *testing.T) {
 	}
 	if p.Host != "0.0.0.0" {
 		t.Error("host")
-	}
-	if p.Mongo.Url != "localhost:1234" {
-		t.Error("mongo.url")
-	}
-	if p.Mongo.Url != "localhost:1234" {
-		t.Error("mongo.url")
-	}
-	if p.Mongo.DatabaseName != "geography" {
-		t.Error("mongo.DatabaseName")
 	}
 
 }
